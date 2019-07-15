19a8ec433ba7b3879d82a1e00f531ed21411f2c2
A7medS94 <a.zaki@smartappco.com>
2019-07-15 13:52:26 +0200

    Initial Commit

---
+++ Key-Value Observing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Key-Value Observing
+//
+//  Created by iMac on 7/15/19.
+//  Copyright © 2019 Ahmed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
