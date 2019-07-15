ffb63d9461000cd9542f6fb2a2e7beceee3a6a45
A7medS94 <a.zaki@smartappco.com>
2019-07-15 14:36:41 +0200

    KVO

--- Key-Value Observing/ViewController.swift
+++ Key-Value Observing/ViewController.swift
@@ -2,19 +2,42 @@
 //  ViewController.swift
 //  Key-Value Observing
 //
-//  Created by iMac on 7/15/19.
+//  Created by Ahmed Samir on 7/15/19.
 //  Copyright © 2019 Ahmed Samir. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var valueChangeLbl: UILabel!
+    @IBOutlet weak var txt: UITextField!
+    
+    //Vars
+    @objc dynamic var value : String?
+    var valueObservation : NSKeyValueObservation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        valueObservation = observe(\ViewController.value,options: .new ,changeHandler: { (strongSelf, change) in
+            guard let new = change.newValue else {return}
+            strongSelf.valueChangeLbl.text = new
+        })
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        valueObservation?.invalidate()
+    }
+    
+    @IBAction func didChangeValueBtn(_ sender: Any) {
+        value = "Btn pressed!"
+    }
+    
+    @IBAction func didChangeTxt(_ sender: Any) {
+        value = self.txt.text
+    }
 }
 
